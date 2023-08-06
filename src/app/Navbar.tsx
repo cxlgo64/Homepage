858d8997d878cece0d8e1441e13785d1411f6ef4
Xueliang Chen <51306962+cxlgo64@users.noreply.github.com>
2023-08-05 21:57:10 -0400

    experience format update

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,31 +1,31 @@
 // ./Navbar.tsx
 import Link from "next/link";
 import React from "react";
-import About from "./page"
+
 const Navbar = () => {
   return (
     <div className="navbar bg-base-500 text-[#fad4a2]">
       <nav className="flex items-center justify-between flex-wrap bg-[#f13c07] p-6">
-  <div className="flex items-center flex-shrink-0  hover:text-white mr-6">
-    <Link href="/" className="block mt-4 lg:inline-block lg:mt-0  mr-4">
-    <span>Xueliang Chen</span>
-      </Link>
-  </div>
-  {/* <div className="block lg:hidden">
-    <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-      <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-    </button>
-  </div> */}
+        <div className="flex items-center flex-shrink-0  hover:text-white mr-6">
+          <div className="" id="menuToggle">
+            <input type="checkbox" className="block lg:hidden md:hidden z-11" />
+             <Link href="/" className="block mt-4 lg:inline-block lg:mt-0  mr-4">
+            <span></span>
+            <span></span>
+            <span></span>
+              <h1 className="lg:block md:block hidden">Xueliang Chen</h1></Link>
+          </div>
+        </div>
   <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div className="text-sm lg:flex-grow">
+    <div className="text-sm lg:flex-grow main-nav" id="menu">
       <Link href="./sample_site" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">
         Website_Examples
       </Link>
       <Link href="./blogs" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">
         Blogs
       </Link>
-      <Link href="./development_kit" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white  mr-4">
-        DevelopKit
+      <Link href="https://github.com/cxlgo64" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white  mr-4">
+        Github
       </Link>
       <Link href="./games" className="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4">
         Games
@@ -34,11 +34,12 @@ const Navbar = () => {
         Contact me
       </Link>
     </div>
-    <div>
+    {/* <div>
       <a href="/chinese" className="inline-block text-sm px-4 py-2 ml-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-[#fad4a2] hover:bg-white mt-4 lg:mt-0">English</a>
-    </div>
+    </div> */}
   </div>
 </nav>
+
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
   return (
     <main>
     <div className="slider_container box-border m-0 p-0  ">
-      <div className="leftSlide ">
-        <div className=' bg-[#7b110b] text-[#fad4a2]'><h2 className='text-3xl p-5'>Galley</h2></div> 
-        <div className=' bg-[#8d1f19] text-[#fad4a2]'><h2 className='text-3xl p-5'>Project Experience</h2></div>
-        <div className=' bg-[#943833] text-[#fad4a2]'><h2 className='text-3xl p-5'>Technology Stack</h2></div>
-        <div className=' bg-[#8c4d4a] text-[#fad4a2] '><h2 className='text-3xl text-left pt-5 pb-5 m-5'>Here is Jonathan(Xueliang) Chen</h2>
+      <div className="leftSlide z-10 ">
+        <div className='  text-[#fad4a2]'><h2 className='text-3xl p-5'>Galley</h2></div> 
+        <div className='  text-[#fad4a2]'><h2 className='text-3xl p-5'>Project Experience</h2></div>
+        <div className='  text-[#fad4a2]'><h2 className='text-3xl p-5'>Technology Stack</h2></div>
+        <div className='  text-[#fad4a2] '><h2 className='lg:text-3xl text-xl text-left pt-5 pb-5 m-5'>Here is Jonathan(Xueliang) Chen</h2>
            <button className='p-20 m-20 text-center rounded-2xl bg-orange-500 border border-transparent px-5 py-4 transition-colors hover:border-gray-300'>
           Download CV
           </button>
@@ -23,7 +23,7 @@ export default function Home() {
 
       </div>
 
-      <div className='rightSlide'>
+      <div className='rightSlide z-8'>
         <div id='Personal' className='right-1 justify-center relative top-1/4 left-20'>
           <Image className="rounded-2xl" 
           src="/DSC03560.jpg"
@@ -37,7 +37,7 @@ export default function Home() {
 
           {/*details for personal technolog stack */}
         <div className='right-2  box-border h-full'>
-          <div className='inLeft float-left gap-y-72 grid-rows-4 grid-flow-col text-2xl'>
+          <div className='inLeft float-left gap-y-72 grid-rows-4 grid-flow-col lg:text-2xl'>
             <div className='py-16 pl-10'><h3>Web Development</h3></div>
             <div className='py-16 pl-10'><h3>Software Development</h3></div>
             <div className='py-16 pl-10'><h3>Game Development</h3></div>
@@ -86,56 +86,64 @@ export default function Home() {
           </div>
        
         {/*details for Project experiences */}
-        <div className='right3 flex mt-10 ml-5'>
-          <Link
-            href="/sample_site"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        <div className='right3 grid-flex-rows mt-4 ml-8 justify-center'>
+          <div className='lg:grid lg:grid-cols-3 lg:h-48 lg:w-[960px]'>
+            <div className="group rounded-lg px-8 border justify-center border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <Link
+              href="/sample_site"
+              className='px-5 py-6 h-1/4'
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Website Example{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Please Check out my website projects with different frameworks and Technology used
+              </p>
+            </Link>
+            </div>
+            <div className="group rounded-lg px-8 justify-center border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <Link
+            href="https://github.com/cxlgo64"
+            className='px-5 py-6 h-1/4'
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
-              Website Example{' '}
+              Github{' '}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Please Check out my website projects with different frameworks and Technology used
+              Check Out My Github repo
             </p>
           </Link>
-          <Link
-          href="https://github.com/cxlgo64"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Github{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Check Out My Github repo
-          </p>
-        </Link>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Game{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Check out the game I involved
-          </p>
-        </a>
+          </div>
+          <div className="group rounded-lg px-8 justify-center border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              className='px-5 py-6'
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={`mb-3 text-2xl font-semibold`}>
+                Game{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Check out the game I involved
+              </p>
+            </a>
+          </div>
+        </div>
+        <div className='grid grid-cols-3 mt-4'>
         {[
             ['Javascript','https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'],
             ['HTML','https://www.w3.org/html/logo/badge/html5-badge-h-solo.png'],
@@ -145,21 +153,21 @@ export default function Home() {
             ['C#','https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png'],
             ['Lightroom','https://upload.wikimedia.org/wikipedia/commons/b/b6/Adobe_Photoshop_Lightroom_CC_logo.svg'],
           ].map(([title,links])=>(
-            <div className="items-center group w-1/2 h-1/4" key={links}>
-              <a key={links} href={links} className='my-2 rounded-2xl'>
-              <Image className="opacity-100 block w-1/2 h-1/2 transition-opacity rounded-2xl group-hover:opacity-30 border-2 p-5" 
+            <div className="items-center group " key={links}>
+              <a key={links} href={links} className=''>
+              <Image className="opacity-100 block w-1/2 h-1/2 transition-opacity rounded-2xl group-hover:opacity-30 border-1 p-5" 
               key={title}
               src={links}
               alt={title}
-              width={300}
-              height={200}
+              width={500}
+              height={500}
               priority
               />
-              <h3 className='transition-opacity opacity-0 w-1/2 group-hover:opacity-100 translate-y-[-30px] lg:translate-y-[-70px] text-center bg-blue-300 text-white text-lg' key={title}>{title}</h3> 
+              <h3 className='transition-opacity opacity-0 my-2 rounded-2xl w-1/2 group-hover:opacity-100 translate-y-[-30px] lg:translate-y-[-70px] text-center bg-blue-300 text-white text-lg' key={title}>{title}</h3> 
              
               </a> 
               </div>   
-          ))}
+          ))}</div>
          
           </div>
         <div className='right-4'><Galley /></div>
